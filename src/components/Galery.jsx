207b95efe10dedc29ba57246e0adef6461728e92
Tomas <tomaasvillagra@gmail.com>
2023-01-08 00:23:03 -0300

    acomodando imagenes BIEN

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import img1 from '../assets/1.jpg'
+import img2 from '../assets/2.jpg'
+import img3 from '../assets/3.jpg'
+import img4 from '../assets/4.jpg'
+import img5 from '../assets/5.jpg'
+import img6 from '../assets/6.jpg'
+import img7 from '../assets/7.jpg'
 
 
 function Galery() {
@@ -8,12 +14,12 @@ return (
     <div className='galeria-titulo'><h4>Galeria</h4></div>
     <div className="grid-container">
         <div className="grid-item" style={{backgroundImage: `url(${img1})`}}></div>
-        <div className="grid-item wide" style={{backgroundImage: 'url(../assets/2.jpg)'}}></div>
-        <div className="grid-item" style={{backgroundImage: 'url(../assets/3.jpg)'}}></div>
-        <div className="grid-item tall" style={{backgroundImage: 'url(../assets/4.jpg)'}}></div>
-        <div className="grid-item" style={{backgroundImage: 'url(../assets/5.jpg)'}}></div>
-        <div className="grid-item wide" style={{backgroundImage: 'url(../assets/6.jpg)'}}></div>
-        <div className="grid-item" style={{backgroundImage: 'url(../assets/7.jpg)'}}></div>
+        <div className="grid-item wide" style={{backgroundImage: `url(${img2})`}}></div>
+        <div className="grid-item" style={{backgroundImage: `url(${img3})`}}></div>
+        <div className="grid-item tall" style={{backgroundImage: `url(${img4})`}}></div>
+        <div className="grid-item" style={{backgroundImage: `url(${img5})`}}></div>
+        <div className="grid-item wide" style={{backgroundImage: `url(${img6})`}}></div>
+        <div className="grid-item" style={{backgroundImage: `url(${img7})`}}></div>
 
     </div>
     </>
