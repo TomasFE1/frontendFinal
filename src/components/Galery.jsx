97ebf6000575f172ce776d8e024e7e23e885236a
Tomas <tomaasvillagra@gmail.com>
2023-01-08 00:11:25 -0300

    moviendo imagenes a assets nuevamente

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -6,13 +6,13 @@ return (
     <>
     <div className='galeria-titulo'><h4>Galeria</h4></div>
     <div className="grid-container">
-        <div className="grid-item" style={{backgroundImage: 'url(../../public/1.jpg)'}}></div>
-        <div className="grid-item wide" style={{backgroundImage: 'url(../../public/2.jpg)'}}></div>
-        <div className="grid-item" style={{backgroundImage: 'url(../../public/3.jpg)'}}></div>
-        <div className="grid-item tall" style={{backgroundImage: 'url(../../public/4.jpg)'}}></div>
-        <div className="grid-item" style={{backgroundImage: 'url(../../public/5.jpg)'}}></div>
-        <div className="grid-item wide" style={{backgroundImage: 'url(../../public/6.jpg)'}}></div>
-        <div className="grid-item" style={{backgroundImage: 'url(../../public/7.jpg)'}}></div>
+        <div className="grid-item" style={{backgroundImage: 'url(../assets/1.jpg)'}}></div>
+        <div className="grid-item wide" style={{backgroundImage: 'url(../assets/2.jpg)'}}></div>
+        <div className="grid-item" style={{backgroundImage: 'url(../assets/3.jpg)'}}></div>
+        <div className="grid-item tall" style={{backgroundImage: 'url(../assets/4.jpg)'}}></div>
+        <div className="grid-item" style={{backgroundImage: 'url(../assets/5.jpg)'}}></div>
+        <div className="grid-item wide" style={{backgroundImage: 'url(../assets/6.jpg)'}}></div>
+        <div className="grid-item" style={{backgroundImage: 'url(../assets/7.jpg)'}}></div>
 
     </div>
     </>
